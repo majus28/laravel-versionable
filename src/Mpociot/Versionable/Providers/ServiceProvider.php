9a84b075ce2f9f9f3c5b2ec685717f12514c7c02
Manokaran Majushan <mano.ceymplon@jeylabs.com.au>
2018-11-01 14:48:01 +0530

    first commit

---
+++ src/Mpociot/Versionable/Providers/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mpociot\Versionable\Providers;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register ()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../../config/config.php', 'versionable');
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../../config/config.php' => config_path('versionable.php'),
+        ], 'config');
+    }
+}
